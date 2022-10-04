b94f4edf1460cd9056acc81deb5ca728710d94a4
K4713 <k4713@localhost.localdomain>
2022-10-05 00:02:48 +0300

    after class 2 of the game

--- script.js
+++ script.js
@@ -1,32 +1,46 @@
 const quote = document.getElementById('quote');
 const input = document.getElementById('typed-value');
 const start = document.getElementById('start');
+const message = document.getElementById('message');
+const quotes = [
+   'Be yourself; everyone else is already taken.',
+   'Im selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you cant handle me at my worst, then you sure as hell dont deserve me at my best.',
+   'Two things are infinite: the universe and human stupidity; and I am not sure about the universe.',
+   'So many books, so little time',
+   'A room without books is like a body without a soul.',
+   'Be who you are and say what you feel, because those who mind do not matter, and those who matter do not mind.'
+]
 
 let wordQueue;
 let highlightPosition;
+let startTime;
 
 function startGame() {
    console.log("Game started!");
+   // get random quote index and set it
+   const quoteIndex = Math.floor(Math.random() * quotes.length);
+   const quoteText = quotes[quoteIndex];
    
-   let quoteText = "K4713 is an amazing human being";
    wordQueue = quoteText.split(' ');
    quote.innerHTML = wordQueue.map(word => (`<span>${word}</span>`)).join('');
 
    highlightPosition = 0;
    quote.childNodes[highlightPosition].className = 'highlight'; 
+   startTime = new Date().getTime();
+   console.log(startTime)
 
-   // quote.childNodes[highlightPosition].className = "";  
-   // highlightPosition++;                           
-   // quote.childNodes[highlightPosition].className = 'highlight'; 
-   // // wordQueue = "Katie";
-   // quote.innerHTML = `<span>${wordQueue}</span>`;
+   document.body.className = "";
+   start.className = "started";
+   message.innerHTML = "";
+   setTimeout(() => {start.className ="button";}, 2000)
+
 }
 start.addEventListener('click', startGame);
 
 function checkInput() { 
    // debugger;
    //  console.log("Checking", input.value);
-    const currentWord = wordQueue[0];
+    const currentWord = wordQueue[0].replaceAll(".", "").replaceAll(",", "");
     const typedValue = input.value.trim();
     if (currentWord !== typedValue){
       input.className = currentWord.startsWith(typedValue) ? "" : "error";
@@ -35,10 +49,22 @@ function checkInput() {
     wordQueue.shift() // 
     input.value = ""; //empty textbox
 
-    quote.childNodes[highlightPosition].className = "";  
+    quote.childNodes[highlightPosition].className = "";
+    if (wordQueue.length === 0) {
+      gameOver();
+      return;
+    }
     highlightPosition++;                           
     quote.childNodes[highlightPosition].className = 'highlight'; 
  
  }
 
 input.addEventListener('input', checkInput);
+
+function gameOver(){
+   console.log("Game over");
+   const elapsedTime = new Date().getTime() - startTime;
+   document.body.className = "winner";
+   message.innerHTML = `<span class="congrats">Congratulations!</span><br>
+   <p class="time">You finished in ${elapsedTime / 1000} seconds </p>`;
+}
